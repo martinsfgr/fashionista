f124c9c1aebb7d6938372343a1f9a7f362360fd2
Mauricio <mauriciofagner2010@hotmail.com>
2020-06-01 14:27:33 -0300

    Estilizar o componente da navbar

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+
+import logoImg from '../assets/logo.png';
+
+function Nav() {
+  return (
+    <nav>
+      <div className="navbar__infos">
+        <img
+          src={logoImg} 
+          alt="Ilustração de um cabide representado a logo da Fashionista"
+          className="company__logo"
+        />
+
+        <p className="company__name">FASHIONISTA</p>
+      </div>
+      
+      <div className="navbar__icons">
+        <FontAwesomeIcon icon={faSearch} className="fa-lg search-icon" />
+        <FontAwesomeIcon icon={faShoppingBag} className="fa-lg cart-icon" />
+      </div>
+    </nav>
+  )
+}
+
+export default Nav;
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Navbar from './components/Nav';
+
 import Home from './pages/Home';
 
 function Routes() {
   return (
     <BrowserRouter>
+    <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
       </Switch>
