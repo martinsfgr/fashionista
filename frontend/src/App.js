7a2cf6853a1125331f41b50fd3e76c89ce63d397
Mauricio <mauriciofagner2010@hotmail.com>
2020-06-18 21:08:22 -0300

    Passar a store para toda aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import Routes from './routes';
+
+import store from './store';
 
 import './styles/css/index.css';
 
 function App() {
   return (
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   );
 }
 
