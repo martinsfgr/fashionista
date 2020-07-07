0706ec3925cef0ddd1703fa7e4dd41817f558725
Mauricio <mauriciofagner2010@hotmail.com>
2020-07-06 23:27:51 -0300

    Melhorar a função que formata o nome do produto

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,10 +1,9 @@
 const formatProductName = (productName) => {
-  let replaceSpaces = productName.replace(/\s/g, '-');
-  let normalizeLetterA = replaceSpaces.replace(/[àáâãä]/g, 'a');
-  let normalizeLetterE = normalizeLetterA.replace(/[èéêẽë]/g, 'e');
-  let normalizeCedilla = normalizeLetterE.replace(/[Çç]/g, 'c');
-  let lowerCaseName = normalizeCedilla.toLowerCase();
-  return lowerCaseName;
+  return productName.replace(/\s/g, '-')
+    .replace(/[àáâãä]/g, 'a')
+    .replace(/[èéêẽë]/g, 'e')
+    .replace(/[Çç]/g, 'c')
+    .toLowerCase();
 }
 
 const formatProductPrice = (productPrice) => {
