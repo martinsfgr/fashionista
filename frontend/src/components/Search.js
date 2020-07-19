4273632b41beb1638705846b6a28a963c04c06b6
Fagner <mauriciofagner2010@hotmail.com>
2020-07-19 20:48:02 -0300

    Ajustes iniciais do componente de busca

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -34,17 +34,24 @@ function Cart () {
 
   return (
     <div className={isSearchActive ? "search__container active" : "search__container"}>
-      <div>
-        <p>Buscar Produto(s)</p>
+      <div className="search__navbar">
         <FontAwesomeIcon 
           icon={faTimes}
-          className="fa-lg times"
+          className="fa-lg times search__close-button"
           onClick={() => handleSearch()}
+          />
+
+          <p className="search__title">Buscar Produto(s)</p>
+      </div>
+
+      <div className="search__input">
+        <input 
+          type="text"
+          placeholder="Buscar por produtos"
+          onChange={(event) => handleChange(event)}
         />
       </div>
-      <div className="search__input">
-        <input type="text" onChange={(event) => handleChange(event)} />
-      </div>
+
       <div className="search__products">
         {products.length > 0 ? products.map(product => {
           return <div key={product.code_color}>
