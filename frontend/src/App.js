321fd393b0ff1acdf1b3249fcaee0fca75afde08
Mauricio <mauriciofagner2010@hotmail.com>
2020-07-30 22:52:42 -0300

    Persistir os dados da store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import Routes from './routes';
 
-import store from './store';
+import { store, persistor }  from './store';
 
 import './styles/css/index.css';
 
 function App() {
   return (
     <Provider store={store}>
-      <Routes />
+      <PersistGate persistor={persistor}>
+        <Routes />
+      </PersistGate>
     </Provider>
   );
 }
